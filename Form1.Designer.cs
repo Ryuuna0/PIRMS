150ccbc51005183bde895e5022ca060789f69c2e
Ryuuna0 <115034951+Ryuuna0@users.noreply.github.com>
2025-04-17 15:00:14 +0200

    Add files via upload
    veci ze seriove linky

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SerialChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.btnOpenClose = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rychlost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nick";
+            // 
+            // cboPort
+            // 
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8"});
+            this.cboPort.Location = new System.Drawing.Point(78, 16);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(159, 21);
+            this.cboPort.TabIndex = 3;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cboBaudRate.Location = new System.Drawing.Point(78, 48);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(159, 21);
+            this.cboBaudRate.TabIndex = 4;
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(78, 79);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(159, 20);
+            this.txtNick.TabIndex = 5;
+            // 
+            // btnOpenClose
+            // 
+            this.btnOpenClose.Location = new System.Drawing.Point(78, 105);
+            this.btnOpenClose.Name = "btnOpenClose";
+            this.btnOpenClose.Size = new System.Drawing.Size(159, 23);
+            this.btnOpenClose.TabIndex = 6;
+            this.btnOpenClose.Text = "Připojit";
+            this.btnOpenClose.UseVisualStyleBackColor = true;
+            this.btnOpenClose.Click += new System.EventHandler(this.btnOpenClose_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(12, 134);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(431, 215);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 355);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(350, 20);
+            this.txtMessage.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(368, 352);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Odeslat";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 391);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnOpenClose);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Serial Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.Button btnOpenClose;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cboPort.Text = Properties.Settings.Default.PortName;
+            cboBaudRate.Text = Properties.Settings.Default.BaudRate;
+            txtNick.Text = Properties.Settings.Default.Nick;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.PortName = cboPort.Text;
+            Properties.Settings.Default.BaudRate = cboBaudRate.Text;
+            Properties.Settings.Default.Nick = txtNick.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnOpenClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnOpenClose.Text == "Připojit")
+                {
+                    serialPort1.PortName = cboPort.Text;
+                    serialPort1.BaudRate = int.Parse(cboBaudRate.Text);
+                    serialPort1.Open();
+                    btnOpenClose.Text = "Odpojit";
+                }
+                else
+                {
+                    serialPort1.Close();
+                    btnOpenClose.Text = "Připojit";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, this.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string start = ((char)0x02).ToString();
+                string end = ((char)0x03).ToString();
+
+                serialPort1.Write(start);
+                serialPort1.Write(txtNick.Text);
+                serialPort1.Write("~");
+                serialPort1.Write(txtMessage.Text);
+                serialPort1.Write(end);
+
+
+                ListViewItem li = new ListViewItem(
+                    txtNick.Text + ": " + txtMessage.Text);
+                li.ForeColor = Color.Pink;
+                listView1.Items.Add(li);
+                txtMessage.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, this.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        string buffer = string.Empty;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen == false)
+                    return;
+                if (serialPort1.BytesToRead == 0)
+                    return;
+
+                buffer += serialPort1.ReadExisting();
+                string[] messages = buffer.Split((char)0x03);
+                buffer = messages[messages.Length - 1];
+
+                for (int i = 0; i < messages.Length - 1; i++)
+                {
+                    string message = messages[i].TrimStart((char)0x02);
+                    string[] vals = message.Split('~');
+                    string nick = vals[0];
+                    string msg = vals[1];
+
+                    ListViewItem li = new ListViewItem(nick + ": " + msg);
+                    li.ForeColor = Color.Blue;
+                    listView1.Items.Add(li);
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                // TODO
+            }
+        }
+    }
+}
